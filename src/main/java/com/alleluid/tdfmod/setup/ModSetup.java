595ed18b1c6bc2948e99232fef74108500127ed6
Alleluid <me@alleluid.com>
2020-04-11 20:43:19 -0700

    Add void protection to principic armor.

--- src/main/java/com/alleluid/tdfmod/setup/ModSetup.java
+++ src/main/java/com/alleluid/tdfmod/setup/ModSetup.java
@@ -3,12 +3,15 @@ package com.alleluid.tdfmod.setup;
 import com.alleluid.tdfmod.TdfMod;
 import com.alleluid.tdfmod.blocks.ModBlocks;
 import com.alleluid.tdfmod.items.armor.AbstractPrincipicArmor;
+import net.minecraft.entity.MoverType;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.item.ItemStack;
 import net.minecraftforge.event.entity.living.LivingDamageEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
+
+import java.util.Objects;
 
 @Mod.EventBusSubscriber(modid = TdfMod.MODID, bus=Mod.EventBusSubscriber.Bus.FORGE)
 public class ModSetup {
@@ -24,11 +27,17 @@ public class ModSetup {
     public static void livingDamage(LivingDamageEvent event) {
         if (event.getEntityLiving() instanceof PlayerEntity){
             PlayerEntity playerEntity = (PlayerEntity) event.getEntityLiving();
-            event.setCanceled(true);
+            boolean fullPrin = true;
             for (ItemStack item: playerEntity.getArmorInventoryList()){
                 if (!(item.getItem() instanceof AbstractPrincipicArmor)) {
-                    event.setCanceled(false);
+                    fullPrin = false;
                 }
+            }
+            if (fullPrin){
+                if (event.getSource().damageType.equals("outOfWorld")) {
+                    playerEntity.setPositionAndUpdate(playerEntity.posX, 260, playerEntity.posZ);
+                }
+                event.setCanceled(true);
             }
         }
     }
