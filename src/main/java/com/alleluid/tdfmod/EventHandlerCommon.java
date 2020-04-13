399cfb551c833bc7c1b6efa60a5da22ada54d52a
Joel Huff <drazonu@gmail.com>
2020-04-12 22:04:51 -0500

    Added rendering layer to use custom Elytra texture

--- src/main/java/com/alleluid/tdfmod/EventHandlerCommon.java
+++ src/main/java/com/alleluid/tdfmod/EventHandlerCommon.java
@@ -1,8 +1,15 @@
 package com.alleluid.tdfmod;
 
+import com.alleluid.tdfmod.client.render.ChestElytraRenderLayer;
 import com.alleluid.tdfmod.items.armor.PrincipicChestplateItem;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.renderer.entity.EntityRendererManager;
+import net.minecraft.client.renderer.entity.LivingRenderer;
+import net.minecraft.client.renderer.entity.model.EntityModel;
+import net.minecraft.entity.LivingEntity;
 import net.minecraftforge.event.entity.living.LivingEquipmentChangeEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.event.lifecycle.FMLLoadCompleteEvent;
 import top.theillusivec4.caelus.api.event.RenderElytraEvent;
 
 public class EventHandlerCommon {
