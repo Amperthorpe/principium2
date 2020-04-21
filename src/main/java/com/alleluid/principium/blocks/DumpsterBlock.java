1f9c83455853e5f004e05d4870234208b754fb3a
Matt <mtthw8198@gmail.com>
2020-04-20 20:21:53 -0700

    Fixed Dumpster scaling / placement

--- src/main/java/com/alleluid/principium/blocks/DumpsterBlock.java
+++ src/main/java/com/alleluid/principium/blocks/DumpsterBlock.java
@@ -1,11 +1,21 @@
 package com.alleluid.principium.blocks;
 
 import net.minecraft.block.Block;
+import net.minecraft.block.BlockState;
 import net.minecraft.block.HorizontalBlock;
 import net.minecraft.block.SoundType;
 import net.minecraft.block.material.Material;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.item.ItemStack;
+import net.minecraft.state.StateContainer;
+import net.minecraft.state.properties.BlockStateProperties;
+import net.minecraft.util.Direction;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
 
-public class DumpsterBlock extends HorizontalBlock
+import javax.annotation.Nullable;
+
+public class DumpsterBlock extends Block
 {
 
     public DumpsterBlock() {
@@ -13,5 +23,23 @@ public class DumpsterBlock extends HorizontalBlock
                 .sound(SoundType.METAL)
                 .hardnessAndResistance(1.5f)
         );
+    }
+
+    @Override
+    public void onBlockPlacedBy(World worldIn, BlockPos pos, BlockState state, @Nullable LivingEntity placer, ItemStack stack) {
+        if (placer != null)
+        {
+            worldIn.setBlockState(pos, state.with(BlockStateProperties.FACING, getFacingFromEntity(pos, placer)), 2);
+        }
+    }
+
+    public static Direction getFacingFromEntity(BlockPos clickedBlock, LivingEntity entity)
+    {
+        return Direction.getFacingFromVector((float)(clickedBlock.getX() - entity.posX), 0, (float)(clickedBlock.getZ() - entity.posZ));
+    }
+
+    @Override
+    protected void fillStateContainer(StateContainer.Builder<Block, BlockState> builder) {
+        builder.add(BlockStateProperties.FACING);
     }
 }
